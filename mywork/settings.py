64836b60b20736de5d87e8a30001b3994b5d35ea
Harsh <harshuttekarweb999@gmail.com>
2026-05-31 18:29:57 +0530

    Fix STATIC_ROOT for render deploy

--- mywork/settings.py
+++ mywork/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -143,3 +142,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
